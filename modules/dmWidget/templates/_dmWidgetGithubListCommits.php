1a51da89bce2d5c4128b4640c2eb779a9eda07d3
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-15 13:17:34 +0100

    improved default templates

--- modules/dmWidget/templates/_dmWidgetGithubListCommits.php
+++ modules/dmWidget/templates/_dmWidgetGithubListCommits.php
@@ -1,4 +1,4 @@
-<?php use_helper('Text');
+<?php use_helper('Text', 'Date');
 
 /*
  * A $commit is an array containing: http://develop.github.com/p/commits.html#listing_commits_on_a_branch
@@ -15,7 +15,10 @@ foreach($commits as $commit)
     ->text(auto_link_text(escape($commit['message'])))
     ->set('.commit_title').
 
-    // render commit user name
+    // render commit date
+    _tag(format_date($commit['committed_date'], 'd/MM H:mm')).
+
+    // render commit author name
     _tag('p.commit_user_name', escape($commit['author']['name']))
   
   );
--- modules/dmWidget/templates/_dmWidgetGithubListIssues.php
+++ modules/dmWidget/templates/_dmWidgetGithubListIssues.php
@@ -1,4 +1,4 @@
-<?php use_helper('Text');
+<?php use_helper('Text', 'Date');
 
 /*
  * An $issue is an array containing: http://develop.github.com/p/issues.html#list_a_projects_issues
@@ -10,10 +10,16 @@ foreach($issues as $issue)
 {
   echo _tag('li',
 
-    // link to the user page on twitter
+    // link to the issue page on github
     _link('http://github.com/'.$user.'/'.$repo.'/'.$state.'#issue/'.$issue['number'])
     ->text($issue['title'])
     ->set('.issue_title').
+
+    // render issue date
+    format_date($issue['created_at'], 'd/MM H:mm').
+
+    // link to the issue user on github
+    _link('http://github.com/'.$user)->text(escape($issue['user'])).
 
     // render issue text
     _tag('p.issue_text', auto_link_text(escape($issue['body'])))
